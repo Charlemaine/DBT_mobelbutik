a6f43b4b90bcb6f22c64cf82c79252f9e106f40f
Charlemaine <89750409+Charlemaine@users.noreply.github.com>
2022-01-31 12:06:11 +0100

    Update DBT_mobelbutik.sql

--- DBT_mobelbutik.sql
+++ DBT_mobelbutik.sql
@@ -1,43 +1,48 @@
+drop database if exists furnitureStore;
 create database furnitureStore;
+use furnitureStore;
 
-create table if not exists customerInfo (
-'customer_id' int (10) not null auto_increment,
-'first_name' varchar (50) not null,
-'last_name' varchar (50) not null,
-'mailaddress' varchar (100) not null,
-'address' varchar (100),
-'telephone' int (20) not null
-)
+create table customerInfo (
+customer_id int (10) not null,
+first_name varchar (50) not null,
+last_name varchar (50) not null,
+mailaddress varchar (100) not null,
+address varchar (100),
+telephone int (20) not null
+);
 
+create table employees(
+empoyee_id int (10) not null,
+first_name varchar (50) not null,
+last_name varchar (50) not null,
+adress varchar (100) not null, 
+mail varchar (100) not null,
+salary float (24)
+);
 
-create table if not exists supplier (
-'supplierID' int (10) not null,
-'supplier_name' varchar (50) not null,
-'telephone' int (20) not null,
-'mail' varchar (100) not null,
-'address' varchar(100)
-)
-
-create table if not exists employees(
-'empoyee_id' int (10) not null,
-'first_name' varchar (50) not null,
-'last_name' varchar (50) not null,
-'adress, mail' varchar (100) not null
-)
+create table orders(
+order_id int(10) not null,
+order_date date not null,
+quantity int (10) not null
+);
 
 create table if not exists products(
-'product_id' int (10) not null,
-'name' varchar (50) not null,
-'stock_Status' int (10) not null
-)
+product_id int (10) not null,
+product_name varchar (50) not null,
+stock_status int (10) not null,
+cost int (10) not null
+);
 
-create table if not exists transactions(
-'transaction_id' int (10) not null,
-'payment_method' varchar (100) not null,
-'salaries' float (24)
-)
+create table suppliers(
+supplier_id int (10) not null,
+supplier_name varchar (50) not null,
+telephone int (20) not null,
+mail varchar (100) not null,
+address varchar(100)
+);
 
-
-create table if not exists transaction_history()
-
-create table if not exists  orders('order_id')
+create table transactions(
+transaction_id int (10) not null,
+payment_method varchar (100) not null,
+transaction_date date not null
+);
