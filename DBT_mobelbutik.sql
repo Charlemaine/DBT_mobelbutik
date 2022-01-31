fa3157f6ad836deb8d7659a4ce0231457147be3b
Charlemaine <89750409+Charlemaine@users.noreply.github.com>
2022-01-31 13:58:14 +0100

    Update DBT_mobelbutik.sql

--- DBT_mobelbutik.sql
+++ DBT_mobelbutik.sql
@@ -9,8 +9,14 @@ first_name varchar (50) not null,
 last_name varchar (50) not null,
 mailaddress varchar (100) not null,
 address varchar (100),
+postal_code varchar(20) not null,
+city varchar(50) not null,
 telephone int (20) not null
 );
+insert into furnitureStore.customerInfo
+values
+(012,'David','Johnson','davidj@mail.com','gatan 23','12345','Örebro',0123456789),
+(013,'John','Davidson','johnd@mail.com','vägen 8','23456','Stockholm',0125436789);
 
 create table employees(
 employee_id int (10) not null,
@@ -40,19 +46,27 @@ stock_status int (10) not null,
 cost int (10) not null
 );
 
+insert into furnitureStore.products
+values
+(123,'Table',30,4000),
+(234,'Chair',50,1700),
+(345,'Couch',23,12000),
+(456,'Bed',2,9000),
+(567,'Carpet',10, 1500),
+(678,'Armchair',5,900);
+
 create table suppliers(
 supplier_id int (10) not null,
 supplier_name varchar (50) not null,
 telephone int (20) not null,
-mail varchar (100) not null,
-address varchar(100)
+mail varchar (100) not null
 );
+insert into furnitureStore.suppliers
+values
+(11,'Happy beds',0123456789,'hb1@happybeds.com'),
+(12,'Sad tables',0135792468,'st1@sadtables.se');
 
 create table transactions(
 transaction_id int (10) not null,
 transaction_date date 
 );
-
-select *
-from furnitureStore.employees
-
